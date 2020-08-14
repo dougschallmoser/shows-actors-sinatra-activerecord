7ebc1c0676b279f0c6d99a1da07bdd1c623e2f3c
Doug Schallmoser <dougschallmoser@mgail.com>
2020-08-14 09:12:53 -0700

    updates controller and renders show and index views

--- Gemfile
+++ Gemfile
@@ -5,10 +5,10 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem 'rake'
-gem 'activerecord'
+gem 'activerecord', '6.0.3.2'
 gem 'sinatra-activerecord'
 gem 'sinatra'
 gem 'pry'
 gem 'sqlite3'
 gem 'require_all'
-gem 'shogun'
+gem 'shotgun'
--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -1,7 +1,22 @@
 class Application < Sinatra::Base
 
+    set :views, "app/views"
+
     get '/' do
+        redirect '/shows'
+    end
+
+    get '/shows' do 
+        @shows = Show.all
         erb :index
+    end
+
+    get '/shows/new' do
+    end
+
+    get '/shows/:id' do 
+        @show = Show.find(params[:id])
+        erb :show 
     end
 
 end 
